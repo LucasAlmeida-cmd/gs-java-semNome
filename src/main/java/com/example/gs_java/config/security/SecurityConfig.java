e56e4271e9249890f4ff999f163f8204b49db044
Lucas Almeida <lucasalmeidafmoraes@gmail.com>
2025-10-21 23:21:24 -0300

    feat: configurando api de login

--- src/main/java/com/example/gs_java/config/security/SecurityConfig.java
+++ src/main/java/com/example/gs_java/config/security/SecurityConfig.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -35,6 +37,10 @@ public class SecurityConfig {
         provider.setUserDetailsService(customUserDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
---
+++ src/main/java/com/example/gs_java/controller/AuthApiController.java
@@ -0,0 +1,37 @@
+﻿package com.example.gs_java.controller;
+
+import com.example.gs_java.config.JwtTokenService;
+import com.example.gs_java.model.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class AuthApiController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+     @Autowired
+     private JwtTokenService jwtTokenService;
+
+    record LoginRequest(String email, String senha) {}
+    record LoginResponse(String token) {}
+    record TokenResponse(String token) {}
+
+    @PostMapping("/login")
+    public ResponseEntity efetuarLogin(@RequestBody @Valid LoginRequest dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email, dados.senha);
+        var authentication = authenticationManager.authenticate(authenticationToken);
+        var tokenJWT = jwtTokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new TokenResponse(tokenJWT));
+    }
+}
