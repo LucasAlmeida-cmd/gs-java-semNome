7c318c3096e773b7135341c249ece1799f4c47c2
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-11-10 08:40:45 -0300

    feat: criação de dto da classe de log para usar no mvc

---
+++ src/main/java/com/example/gs_java/dtos/LogDiarioRequestDTO.java
@@ -0,0 +1,34 @@
+package com.example.gs_java.dtos;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class LogDiarioRequestDTO {
+
+
+    @NotNull(message = "A data não pode ser nula")
+    private LocalDate data;
+
+    @NotBlank(message = "O campo emoção é obrigatório")
+    private String emocao;
+
+    @NotNull(message = "Horas de sono é obrigatório")
+    @Min(value = 0, message = "Valor mínimo de sono é 0")
+    @Max(value = 24, message = "Valor máximo de sono é 24")
+    private Integer horasSono;
+
+    @NotNull(message = "Litros de água é obrigatório")
+    @PositiveOrZero(message = "A quantidade de água não pode ser negativa")
+    private Double aguaLitros;
+
+    @NotNull(message = "O campo 'fez exercício' é obrigatório")
+    private Boolean fezExercicio;
+
+    @NotNull(message = "O campo 'descansou a mente' é obrigatório")
+    private Boolean descansouMente;
+
+    private String notas;
+    
+}
