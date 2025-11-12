ab190ee0f53f61e1d843d27cacf9e1eddddde2f9
Lucas Almeida <lucasalmeidafmoraes@gmail.com>
2025-11-11 22:15:09 -0300

    feat: end-point para atualizar usuario

--- src/main/java/com/example/gs_java/controller/api/UsuarioAPIController.java
+++ src/main/java/com/example/gs_java/controller/api/UsuarioAPIController.java
@@ -5,10 +5,7 @@ import com.example.gs_java.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -22,6 +19,12 @@ public class UsuarioAPIController {
     @PostMapping("/salvar")
     public ResponseEntity<Usuario> salvarUsuario(@RequestBody @Valid Usuario usuario) {
         return ResponseEntity.ok(usuarioService.adicionar(usuario));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Void> atualizarUsuario(@PathVariable Long id, @RequestBody @Valid Usuario usuario) {
+        usuarioService.atualizar(id, usuario);
+        return ResponseEntity.noContent().build();
     }
 
 
